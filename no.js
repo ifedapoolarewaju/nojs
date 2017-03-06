beb6b13473807e10543ce8bfea6fe9c16f9b9689
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-03-06 01:32:45 +0100

    feature: target self

--- no.js
+++ no.js
@@ -18,10 +18,17 @@
           var otherParts = attr.value.split(' ');
 
           var eventType = parts[1], action = parts[2], propertyType = parts[3];
-          var target = otherParts[0];
+
+          if (parts.length === 5 && parts[4] === 'self') {
+            var target = el;
+            var optionalArgs = otherParts.slice(0);
+          } else {
+            var target = otherParts[0];
+            var optionalArgs = otherParts.slice(1);
+          }
 
           el.addEventListener(eventType, function(e){
-            this_._handler(action, target, propertyType, otherParts.slice(1));
+            this_._handler(action, target, propertyType, optionalArgs);
           })
         }
       })
@@ -31,7 +38,9 @@
   // propertyType can be attribute, class, id, dom
   // action can be add or remove
   NoJS.prototype._handler = function (action, target, propertyType, optionalArgs) {
-    document.querySelectorAll(target).forEach(function(el){
+    var targets = typeof target === "string" ? document.querySelectorAll(target) : [target];
+
+    targets.forEach(function(el){
       if (propertyType === 'class') {
         el.classList[action](optionalArgs[0])
       }
@@ -45,7 +54,6 @@
           var propertyValue = propertyType === 'id' ? optionalArgs[0] : agrs[1];
           el.setAttribute(propertyName, propertyValue);
         }
-
       }
 
       else if (propertyType === 'dom' && action === 'remove') {
